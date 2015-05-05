de11d5310bd9bfb0af51d96181b18fce82ca1024
Benson Che <bche@resdat.com>
2015-05-04 22:49:38 -0800

    init

---
+++ ProjVerify/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProjVerify
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            var result = dialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                txtCsproj.Text = dialog.FileName;
+
+                txtDir.Text = Path.GetDirectoryName(dialog.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtDir.Text = dialog.SelectedPath;
+            }
+
+        }
+    }
+}
