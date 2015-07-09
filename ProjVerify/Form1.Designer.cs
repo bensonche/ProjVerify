cd70f44e35efd63dbe27431c27a3aec5a4340b78
Benson Che <bche@resdat.com>
2015-07-08 19:08:13 -0700

    Fix title

--- ProjVerify/Form1.Designer.cs
+++ ProjVerify/Form1.Designer.cs
@@ -134,7 +134,7 @@
             this.Controls.Add(this.label1);
             this.MinimumSize = new System.Drawing.Size(300, 300);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ProjVerify";
             this.ResumeLayout(false);
             this.PerformLayout();
 
